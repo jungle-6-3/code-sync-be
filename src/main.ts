dc896bf1394bacf8e29b29f2d3ab1a7c94d86497
HyoungukJo <whguddnr9@gmail.com>
2024-10-19 14:10:52 +0900

    feat: init swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Code-Sync')
+    .setDescription('The Code-Sync API description')
+    .setVersion('1.0')
+    .addTag('main')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
