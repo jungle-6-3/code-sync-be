ad6f1288bc8294000bacf9e6bc22af6579108111
HyoungukJo <whguddnr9@gmail.com>
2024-10-22 13:31:17 +0900

    feat: make validate-globalpipe

---
+++ src/validate-global.pipe.ts
@@ -0,0 +1,36 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import { validDataException } from './auth/auth.exceptions';
+
+/*
+ * catch class-validator exception
+ * Define a custom error type ex)authError
+ * useage : UsePipe(ValidateGlobalPipe(authError))
+ */
+@Injectable()
+export class ValidateGlobalPipe implements PipeTransform {
+  private errorType;
+  constructor(errorType) {
+    this.errorType = errorType;
+  }
+  async transform(value: any, metadata: ArgumentMetadata) {
+    console.log(value);
+
+    const validPipe = new ValidationPipe({
+      disableErrorMessages: true,
+      exceptionFactory: (error: ValidationError[]) => {
+        const properties = error.map((error) => error.property);
+        const { code, message } = this.errorType[properties[0]];
+        throw new validDataException(message, code);
+        console.log(code);
+      },
+    });
+    const val = await validPipe.transform(value, metadata);
+    return val;
+  }
+}
