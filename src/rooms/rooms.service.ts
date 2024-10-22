7d162a1ec33ce3dfb326e3ecf77c357554dc7935
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 02:00:17 +0900

    feat: add constructor of rooms

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Room } from './room';
 
 @Injectable()
 export class RoomsService {
+  private rooms: Map<string, Room>;
+
+  constructor() {
+    this.rooms = new Map<string, Room>();
+  }
+
   async createRoom(creatorPk: number, prUrl: string): Promise<string> {
     return 'This action adds a new room';
   }
