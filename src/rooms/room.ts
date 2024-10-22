26b9bd0e3bae09e7cb7f77411804a0d98f518135
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 01:12:27 +0900

    feat: add room for save information of chatting room

---
+++ src/rooms/room.ts
@@ -0,0 +1,35 @@
+export enum RoomStatus {
+  WATING = 'Wating',
+  RUNNING = 'Running',
+  CLOSING1 = 'Closing1',
+  CLOSING2 = 'Closing2',
+  DELETED = 'Deleted',
+}
+
+export class Room {
+  uuid: string;
+
+  status: RoomStatus;
+
+  creatorPk: string;
+
+  participantPk: string;
+
+  startedAt: Date;
+
+  finishedAt: Date;
+
+  prUrl: string;
+
+  // Not maid yet.
+  // Should define after conversations done
+  data: any;
+
+  constructor(uuid: string, creatorPk: string, prUrl: string) {
+    this.uuid = uuid;
+    this.status = RoomStatus.WATING;
+    this.creatorPk = creatorPk;
+    this.prUrl = prUrl;
+    this.startedAt = new Date();
+  }
+}
