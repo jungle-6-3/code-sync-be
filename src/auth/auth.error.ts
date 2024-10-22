29feb4b78b497bc82766760e0e45e78eda5133dc
HyoungukJo <whguddnr9@gmail.com>
2024-10-22 13:43:16 +0900

    feat: create custom eception

---
+++ src/auth/auth.error.ts
@@ -0,0 +1,5 @@
+export const authError = {
+  name: { code: 'AUTH_1', message: '이름 형식이 맞지 않습니다.' },
+  email: { code: 'AUTH_6', message: '이메일 형식이 맞지 않습니다.' },
+  password: { code: 'AUTH_7', message: '비밀번호 형식이 맞지 않습니다.' },
+};
