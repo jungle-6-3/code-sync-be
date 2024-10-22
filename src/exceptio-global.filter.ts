7f3c6f2445522e456d2378674dbea47398e86f72
HyoungukJo <whguddnr9@gmail.com>
2024-10-23 01:16:23 +0900

    fix: remove debug console.log

--- src/exceptio-global.filter.ts
+++ src/exceptio-global.filter.ts
@@ -10,7 +10,6 @@ import { validDataException } from './auth/auth.exceptions';
 @Catch()
 export class ExceptioGlobalFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
-    console.log(exception)
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
