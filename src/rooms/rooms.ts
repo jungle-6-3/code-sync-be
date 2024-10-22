f94e5b0704a93049b70a1aa0623624760a65ed86
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 02:39:13 +0900

    feat: add method for rooms

--- src/rooms/rooms.ts
+++ src/rooms/rooms.ts
@@ -2,24 +2,36 @@ import { Room } from './room';
 
 export class Rooms {
   private roomsById: Map<string, Room>;
-  private roomsByCreator: Map<number, Room>;
-  private roomsByParticipant: Map<number, Room>;
+  private roomsByPk: Map<number, Room>;
 
   constructor() {
     this.roomsById = new Map();
-    this.roomsByCreator = new Map();
-    this.roomsByParticipant = new Map();
+    this.roomsByPk = new Map();
   }
 
   async findRoombyUuid(uuid: string): Promise<Room> {
     return this.roomsById.get(uuid);
   }
 
-  async findRoombyCreator(creatorPk: number): Promise<Room> {
-    return this.roomsByCreator.get(creatorPk);
+  async findRoombyPk(userPk: number): Promise<Room> {
+    return this.roomsByPk.get(userPk);
   }
 
-  async findRoomByParticipan(participanPk: number): Promise<Room> {
-    return this.roomsByParticipant.get(participanPk);
+  async setRoom(uuid: string, room: Room) {
+    this.roomsById.set(uuid, room);
+  }
+
+  async joinRoom(userPk: number, room: Room) {
+    this.roomsByPk.set(userPk, room);
+  }
+
+  async leaveRoom(userPk: number) {
+    if (userPk) this.roomsByPk.delete(userPk);
+  }
+
+  async deleteRoom(room: Room) {
+    this.leaveRoom(room.creatorPk);
+    this.leaveRoom(room.participantPk);
+    this.roomsById.delete(room.uuid);
   }
 }
