fbbe1847eafb576c517792d529ebb6ed27f920f1
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 02:47:58 +0900

    feat: add feat of create room

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { Room } from './room';
+import { v4 as _uuid } from 'uuid';
+import { Rooms } from './rooms';
 
 @Injectable()
 export class RoomsService {
-  private rooms: Map<string, Room>;
+  private rooms: Rooms;
 
   constructor() {
-    this.rooms = new Map<string, Room>();
+    this.rooms = new Rooms();
   }
 
   async createRoom(creatorPk: number, prUrl: string): Promise<string> {
-    return 'This action adds a new room';
+    if (await this.rooms.findRoombyPk(creatorPk)) {
+      throw new ForbiddenException(
+        '이미 대화에 참여하고 있는 방이 존재합니다.',
+      );
+    }
+    const uuid = _uuid();
+    const newRoom = new Room(uuid, creatorPk, prUrl);
+    this.rooms.setRoom(uuid, newRoom);
+    return `https://code-sync.net/${uuid}`;
   }
 
   async saveRoom(creatorPk: number, roomUuid: string): Promise<boolean> {
