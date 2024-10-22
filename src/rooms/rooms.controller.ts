ba1588d54805cf78c7f62e162bc912cf3c894e09
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 01:27:39 +0900

    feat: add createRoom in room controller

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Param } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
-import { CreateRoomDto } from './dto/create-room.dto';
-import { UpdateRoomDto } from './dto/update-room.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtPayloadDto } from 'src/auth/dto/jwt-payload';
+import { UsersService } from 'src/users/users.service';
 
-@Controller('rooms')
+@Controller('room')
 export class RoomsController {
-  constructor(private readonly roomsService: RoomsService) {}
+  constructor(
+    private readonly roomsService: RoomsService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':prUrl')
+  async createRoom(
+    @Param('prUrl') prUrl: string,
+    @Request() req: Request & { user: JwtPayloadDto },
+  ) {
+    const userPk = await this.usersService.findUserPk(req.user);
+    return this.roomsService.createRoom(userPk, prUrl);
+  }
 
   @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
-    return this.roomsService.create(createRoomDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.roomsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roomsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
-    return this.roomsService.update(+id, updateRoomDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomsService.remove(+id);
-  }
+  saveRoom() {}
 }
