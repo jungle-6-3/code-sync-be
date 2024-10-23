77741bfb97e6a6c1b10a3fa4b645eafa09637d72
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 17:29:54 +0900

    refector: RoomSocket에 일부 정보 대신 User로 치환

--- src/conversation-events/interfaces/room-socket.interface.ts
+++ src/conversation-events/interfaces/room-socket.interface.ts
@@ -3,15 +3,9 @@ import { User } from 'src/users/entities/user.entity';
 
 export interface RoomSocket extends Socket {
   roomUuid: string;
-  userPk: number;
-  email: string;
-  name: string;
+  user: User;
 }
 
 export function initRoomSocket(socket: RoomSocket, user: User) {
-  if (user != undefined) {
-    socket.userPk = user.pk;
-    socket.email = user.email;
-    socket.name = user.name;
-  }
+  socket.user = user;
 }
