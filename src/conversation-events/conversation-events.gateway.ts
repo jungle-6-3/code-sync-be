d64b05dd9e5e88f2fb9103d135f474668f5f7f17
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 14:49:01 +0900

    feat: add skeleton code about handling init, connect, disconnect on server

--- src/conversation-events/conversation-events.gateway.ts
+++ src/conversation-events/conversation-events.gateway.ts
@@ -1,9 +1,37 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ConversationEventsGateway {
+export class ConversationEventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor() {}
+
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('RoomEventGateway');
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Initialize WebSocket Server Done');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client Disconnected : ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client Connected : ${client.id}`);
   }
 }
