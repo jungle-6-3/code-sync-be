a92e71d006bd92f70345384ea01ca7a5f79f8588
pig19980 <pig19980@kaist.ac.kr>
2024-10-23 16:39:58 +0900

    fix: add imports in conversation-events

--- src/conversation-events/conversation-events.module.ts
+++ src/conversation-events/conversation-events.module.ts
@@ -3,9 +3,17 @@ import { ConversationEventsGateway } from './conversation-events.gateway';
 import { ChattingGateway } from './events/chatting.gateway';
 import { SharedDocGateway } from './events/shared-doc.gateway';
 import { VoiceGateway } from './events/voice.gateway';
+import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [UsersModule, AuthModule],
   controllers: [],
-  providers: [ConversationEventsGateway, ChattingGateway, SharedDocGateway, VoiceGateway],
+  providers: [
+    ConversationEventsGateway,
+    ChattingGateway,
+    SharedDocGateway,
+    VoiceGateway,
+  ],
 })
 export class ConversationEventsModule {}
