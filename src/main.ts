ffa3afd5d5a6f8cda3002803cbeb58e0123247bc
HyoungukJo <whguddnr9@gmail.com>
2024-10-24 21:35:45 +0900

    fix: add credentials true

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ExceptionGlobalFilter } from './utils/exception-global.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:5173'] });
+  app.enableCors({ origin: ['http://localhost:5173'], credentials: true });
   // use cookie-parser middleware as global
   app.use(cookieParser());
   app.setGlobalPrefix('api');
