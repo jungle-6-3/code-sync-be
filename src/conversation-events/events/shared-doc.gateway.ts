c66f814806ada0b6cab1aa34844c2bfb36b5cc55
pig19980 <pig19980@kaist.ac.kr>
2024-10-24 10:58:39 +0900

    fix: try socket cors error

--- src/conversation-events/events/shared-doc.gateway.ts
+++ src/conversation-events/events/shared-doc.gateway.ts
@@ -1,6 +1,13 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
-@WebSocketGateway()
+@WebSocketGateway(3001, {
+  cors: {
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'OPTION', 'FETCH', 'Put', 'DELETE'],
+    credentials: true,
+    transports: ['polling', 'websocket'],
+  },
+})
 export class SharedDocGateway {
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
