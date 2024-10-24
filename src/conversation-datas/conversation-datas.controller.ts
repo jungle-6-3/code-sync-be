a1f3c7e6acf1c6e8746dc84d7368987e144e4ab7
HyoungukJo <whguddnr9@gmail.com>
2024-10-24 23:50:25 +0900

    feat: 파일저장 API를 만든다.

--- src/conversation-datas/conversation-datas.controller.ts
+++ src/conversation-datas/conversation-datas.controller.ts
@@ -7,7 +7,9 @@ import {
 } from '@nestjs/common';
 import { ConversationDatasService } from './conversation-datas.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Conversation-datas')
 @Controller('conversation-datas')
 export class ConversationDatasController {
   constructor(private conversationDatasService: ConversationDatasService) {}
@@ -16,6 +18,11 @@ export class ConversationDatasController {
     return 'hello';
   }
 
+  @ApiOperation({
+    summary: 'File upload',
+    description:
+      '파일 업로드를 요청한다. (클라이언트에서 영상을 올릴 경우를 대비)',
+  })
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
@@ -24,7 +31,7 @@ export class ConversationDatasController {
         throw new Error('파일이 없습니다.');
       }
 
-      const result = await this.conversationDatasService.uploadFile(file);
+      const result = await this.conversationDatasService.uploadClient(file);
 
       return {
         statusCode: 200,
@@ -43,5 +50,4 @@ export class ConversationDatasController {
       };
     }
   }
-  
 }
