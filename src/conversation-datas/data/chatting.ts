277034332bbdc3fed31cb84f12f63826a5b36725
HyoungukJo <whguddnr9@gmail.com>
2024-10-25 21:43:21 +0900

    fix: queue에 저장할 chat을 인자로 선언

--- src/conversation-datas/data/chatting.ts
+++ src/conversation-datas/data/chatting.ts
@@ -1,5 +1,10 @@
 export class ChatData {
   private chats: chat[];
+  uuid: string;
+
+  public addChat(chat: chat) {
+    this.chats.push(chat);
+  }
 
   public toString() {
     return JSON.stringify(this.chats);
