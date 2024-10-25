2a568086fd239f68dd5a4d9ed62678401a719f98
pig19980 <pig19980@kaist.ac.kr>
2024-10-25 18:42:18 +0900

    refector: RoomSocket이 socket의 모든 정보를 담도록 수정

--- src/conversation-events/interfaces/room-socket.interface.ts
+++ src/conversation-events/interfaces/room-socket.interface.ts
@@ -1,16 +1,33 @@
 import { Socket } from 'socket.io';
+import { Room } from 'src/rooms/room';
 import { User } from 'src/users/entities/user.entity';
+
+export enum SocketStatus {
+  CREATOR = 'Creator',
+  WAITER = 'Waiter',
+  PARTICIPANT = 'Participant',
+}
 
 export interface RoomSocket extends Socket {
   user: User;
-  roomUuid: string;
+  room: Room;
+  status: SocketStatus;
+  peerId: string;
 }
 
-export function initRoomSocket(
-  socket: RoomSocket,
-  user: User,
-  roomUuid: string,
-) {
+export function initRoomSocket(socket: RoomSocket, user: User, room: Room) {
   socket.user = user;
-  socket.roomUuid = roomUuid;
+  socket.room = room;
+}
+
+export function setToCreator(socket: RoomSocket) {
+  socket.status = SocketStatus.CREATOR;
+}
+
+export function setToWaiter(socket: RoomSocket) {
+  socket.status = SocketStatus.WAITER;
+}
+
+export function setToParticipant(socket: RoomSocket) {
+  socket.status = SocketStatus.PARTICIPANT;
 }
