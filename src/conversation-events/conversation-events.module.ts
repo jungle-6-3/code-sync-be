0ac791cc75863441eb635c6f82441dc5fda73365
pig19980 <pig19980@kaist.ac.kr>
2024-10-25 20:31:57 +0900

    refector: ConversationEvents의 service file 위치 수정

--- src/conversation-events/conversation-events.module.ts
+++ src/conversation-events/conversation-events.module.ts
@@ -3,10 +3,18 @@ import { ConversationEventsGateway } from './conversation-events.gateway';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { RoomsModule } from 'src/rooms/rooms.module';
+import { RoomEventsHandlerService } from './services/room-events-handler.service';
+import { ConversationEventsLoggerService } from './services/conversation-events-logger.service';
+import { PeerJsEventsHandlerService } from './services/peer-js-events-handler.service';
 
 @Module({
   imports: [UsersModule, AuthModule, RoomsModule],
   controllers: [],
-  providers: [ConversationEventsGateway],
+  providers: [
+    ConversationEventsGateway,
+    RoomEventsHandlerService,
+    ConversationEventsLoggerService,
+    PeerJsEventsHandlerService,
+  ],
 })
 export class ConversationEventsModule {}
