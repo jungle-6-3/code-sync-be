9cc0f34ad5c94c8ad13b9fd870371a6a2e51fded
pig19980 <pig19980@kaist.ac.kr>
2024-10-25 21:06:52 +0900

    refector: PeerJs에 logger 추가

--- src/conversation-events/services/peer-js-events-handler.service.ts
+++ src/conversation-events/services/peer-js-events-handler.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RoomSocket } from '../interfaces/room-socket.interface';
 import { Server } from 'socket.io';
+import { ConversationEventsLoggerService } from './conversation-events-logger.service';
 
 @Injectable()
 export class PeerJsEventsHandlerService {
+  constructor(private loggerService: ConversationEventsLoggerService) {}
+  private logger: Logger = this.loggerService.getLogger();
+
   async sharePeerIdHandler(server: Server, client: RoomSocket, peerId: string) {
     const room = client.room;
     server.to(room.uuid).emit('new-peer-id', {
