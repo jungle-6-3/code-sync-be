1df8223615e89a72ccdbd7d3170e8a388dde8f97
pig19980 <pig19980@kaist.ac.kr>
2024-10-26 20:35:53 +0900

    fix: ValidateUserIsCreatorPipe의 에러 이름 수정

--- src/conversation-events/pipes/validate-user-is-creator.pipe.ts
+++ src/conversation-events/pipes/validate-user-is-creator.pipe.ts
@@ -7,7 +7,7 @@ export class ValidateUserIsCreatorPipe implements PipeTransform {
   transform(value: RoomSocket, metadata: ArgumentMetadata) {
     if (value.constructor && value.constructor.name == 'Socket') {
       if (value.status != SocketStatus.CREATOR) {
-        throw new WsException(`방에 참여하지 않았습니다.: ${value.status}`);
+        throw new WsException(`방장이 아닙니다.: ${value.status}`);
       }
     }
     return value;
