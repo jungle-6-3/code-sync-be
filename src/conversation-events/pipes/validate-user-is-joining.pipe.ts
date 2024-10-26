9ae0ae14e3fcadc0f62714b85a6ec7d42f794084
pig19980 <pig19980@kaist.ac.kr>
2024-10-26 20:24:28 +0900

    feat: ValidateUserIsJoiningPipe에서 room의 상태도 확인하도록 수정

--- src/conversation-events/pipes/validate-user-is-joining.pipe.ts
+++ src/conversation-events/pipes/validate-user-is-joining.pipe.ts
@@ -1,16 +1,23 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { RoomSocket, SocketStatus } from '../interfaces/room-socket.interface';
 import { WsException } from '@nestjs/websockets';
+import { RoomStatus } from 'src/rooms/room';
 
 @Injectable()
 export class ValidateUserIsJoiningPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: RoomSocket, metadata: ArgumentMetadata) {
     if (value.constructor && value.constructor.name == 'Socket') {
       if (
         value.status != SocketStatus.CREATOR &&
         value.status != SocketStatus.PARTICIPANT
       ) {
         throw new WsException('방에 참여하지 않았습니다.');
+      }
+      const room = value.room;
+      if (room.status != RoomStatus.RUNNING) {
+        throw new WsException(
+          `방의 status가 Running이 아닙니다: ${room.status}`,
+        );
       }
     }
     return value;
