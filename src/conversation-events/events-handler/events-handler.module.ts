3c51073b9d10f4a40c7604f9e92a9d03af823393
HyoungukJo <whguddnr9@gmail.com>
2024-10-28 22:41:57 +0900

    fix: add chattingHandle Gateway to  event module

--- src/conversation-events/events-handler/events-handler.module.ts
+++ src/conversation-events/events-handler/events-handler.module.ts
@@ -4,10 +4,11 @@ import { UsersModule } from 'src/users/users.module';
 import { RoomsModule } from 'src/rooms/rooms.module';
 import { PeerJsGateway } from './peer-js.gateway';
 import { RoomGateway } from './room.gateway';
+import { ChattingHandlerGateway } from './chatting-handler.gateway';
 
 @Module({
   imports: [AuthModule, UsersModule, RoomsModule],
-  providers: [PeerJsGateway, RoomGateway],
+  providers: [PeerJsGateway, RoomGateway, ChattingHandlerGateway],
   exports: [],
 })
 export class EventsHandlerModule {}
