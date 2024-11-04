91f87a5a4c6bbaa2c964b24d112b11615f67440b
pig19980 <pig19980@kaist.ac.kr>
2024-11-04 18:38:44 +0900

    refactor: drawBoard에 module 제거

--- src/conversation-datas/data/drawBoard.ts
+++ src/conversation-datas/data/drawBoard.ts
@@ -1,13 +1,8 @@
-import { Module } from '@nestjs/common';
 import { SaveDataDtoConversation } from './save-data-dto-convesation.interface';
 import * as Y from 'yjs';
-import { YjsModule } from 'src/yjs/yjs.module';
 import { YjsService } from 'src/yjs/yjs.service';
 import { YjsDocProvider } from 'src/yjs/yjs-doc-provider.interface';
 
-@Module({
-  imports: [YjsModule],
-})
 export class drawBoard implements SaveDataDtoConversation {
   private doc: Y.Doc;
 
