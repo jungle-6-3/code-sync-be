8c5124e7effb8bb4669b2cf18a0f9e95fb8bc256
pig19980 <pig19980@kaist.ac.kr>
2024-11-04 13:11:03 +0900

    refactor: saveRoom시 dataPk를 가져오게 하는 service 구현

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -3,13 +3,21 @@ import { Room } from './item';
 import { v4 as _uuid } from 'uuid';
 import { User } from 'src/users/entities/user.entity';
 import { RoomEventService } from './item/room-event/room-event.service';
+import { ConversationsService } from 'src/conversations/conversations.service';
+import { RoomSaveDto } from 'src/conversations/dto/room-save.dto';
+import { UsersService } from 'src/users/users.service';
+import { ConversationDataSaveDto } from 'src/conversation-datas/dto/conversation-data-save.dto';
 
 @Injectable()
 export class RoomsService {
   private roomsById: Map<string, Room>;
   logger: Logger = new Logger('RoomsService');
 
-  constructor(private roomEventsService: RoomEventService) {
+  constructor(
+    private roomEventsService: RoomEventService,
+    private conversationsService: ConversationsService,
+    private usersService: UsersService,
+  ) {
     this.roomsById = new Map();
   }
 
@@ -24,11 +32,25 @@ export class RoomsService {
   }
 
   // TODO: room 내용을 저장하는 행위를 해야함.
-  async saveRoom(room: Room, creator: User) {
-    this.logger.log(`${creator.name}가 ${room.uuid}를 저장`);
-    // 저장하는 행위....
-    this.roomEventsService.deleteRoom(room);
-    return true;
+  async saveRoom(room: Room) {
+    const dataPk = 0;
+    const { creatorPk, participantPk, startedAt, finishedAt } = room;
+    const participant = await this.usersService.fineOneByPk(room.participantPk);
+    const title = `${participant.name}와의 대화`;
+    const data: ConversationDataSaveDto = { canShared: false };
+    const roomSaveDto: RoomSaveDto = {
+      creatorPk,
+      participantPk,
+      title,
+      startedAt,
+      finishedAt,
+      data,
+    };
+
+    const conversation =
+      await this.conversationsService.createConversation(roomSaveDto);
+
+    return conversation.dataPk;
   }
 
   async findRoomByUuid(uuid: string) {
