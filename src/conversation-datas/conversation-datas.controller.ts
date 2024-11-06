3e992904fb1a5c97b9a0aa13f47ec8b4ac64fea9
HyoungukJo <whguddnr9@gmail.com>
2024-11-06 10:42:34 +0900

    feat: 테스트용 api 작성

--- src/conversation-datas/conversation-datas.controller.ts
+++ src/conversation-datas/conversation-datas.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
 import { ConversationDatasService } from './conversation-datas.service';
 
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateConversationDataDto } from './dto/update-conversation-data.dto';
 
 @ApiTags('Conversation-datas')
 @Controller('conversation-datas')
-export class ConversationDatasController {}
+export class ConversationDatasController {
+  constructor(private conversationDataService: ConversationDatasService) {}
+  @Patch(':dataPk')
+  async testApi(
+    @Param('dataPk') dataPk: number,
+    @Body() updateConversationDataDto: UpdateConversationDataDto,
+  ) {
+    return await this.conversationDataService.updateConversatoinDatas(
+      updateConversationDataDto,
+      dataPk,
+    );
+  }
+}
