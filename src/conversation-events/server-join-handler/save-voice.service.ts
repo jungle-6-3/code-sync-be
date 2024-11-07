9c2bf6b2caaa5188424211f0e1c8b3f409ea4465
pig19980 <pig19980@kaist.ac.kr>
2024-11-07 21:39:22 +0900

    refactor: ServerJoinHandlerModule에서 SaveVoiceService 삭제

--- src/conversation-events/server-join-handler/save-voice.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Room } from 'src/rooms/item';
-
-@Injectable()
-export class SaveVoiceService {
-  async startSaveVoice(room: Room) {}
-
-  async finishSaveVoice(room: Room) {}
-}
--- src/conversation-events/server-join-handler/server-join-handler.module.ts
+++ src/conversation-events/server-join-handler/server-join-handler.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServerJoinHandlerService } from './server-join-handler.service';
 import { SaveYjsService } from './save-yjs.service';
-import { SaveVoiceService } from './save-voice.service';
 import { YjsModule } from 'src/yjs/yjs.module';
 
 @Module({
   imports: [YjsModule],
-  providers: [ServerJoinHandlerService, SaveVoiceService, SaveYjsService],
+  providers: [ServerJoinHandlerService, SaveYjsService],
   exports: [ServerJoinHandlerService],
 })
 export class ServerJoinHandlerModule {}
