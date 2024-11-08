0ce4e5b729a56f58bd8a75480e092efe73a0d5ae
HyoungukJo <whguddnr9@gmail.com>
2024-11-08 21:21:32 +0900

    refactor: 회의록 수정 api converseation으로 이동

--- src/conversation-datas/conversation-datas.controller.ts
+++ src/conversation-datas/conversation-datas.controller.ts
@@ -1,32 +1,9 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  UseGuards,
-  Request,
-} from '@nestjs/common';
-import { ConversationDatasService } from './conversation-datas.service';
+import { Controller, UseGuards } from '@nestjs/common';
 
 import { ApiTags } from '@nestjs/swagger';
-import { UpdateConversationDataDto } from './dto/update-conversation-data.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { JwtPayloadDto } from 'src/auth/dto/jwt-payload';
 
 @ApiTags('Conversation-datas')
 @Controller('conversation-datas')
 @UseGuards(JwtAuthGuard)
-export class ConversationDatasController {
-  constructor(private conversationDataService: ConversationDatasService) {}
-  @Patch(':dataPk')
-  async testApi(
-    @Param('dataPk') dataPk: number,
-    @Body() updateConversationDataDto: UpdateConversationDataDto,
-  ) {
-    return await this.conversationDataService.updateConversatoinDatas(
-      updateConversationDataDto,
-      dataPk,
-    );
-  }
-}
+export class ConversationDatasController {}
