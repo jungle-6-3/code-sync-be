8060fce197b9c910b2673c4cb43a6569a7339512
pig19980 <pig19980@kaist.ac.kr>
2024-11-09 15:13:55 +0900

    refactor: promt 내용 이동

--- src/open-ai/open-ai.service.ts
+++ src/open-ai/open-ai.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { OpenAI } from 'openai';
+import * as Promt from './prompt';
 
 @Injectable()
 export class OpenAiService {
@@ -17,14 +18,7 @@ export class OpenAiService {
     try {
       const response = await this.openai.chat.completions.create({
         model: 'gpt-4o-mini',
-        messages: [
-          {
-            role: 'system',
-            content:
-              '너는 openai에 대해 설명해주는 비서야. 주어진 text를 보고 할 수 있는 일에 대해 말해줘.',
-          },
-          { role: 'user', content: text },
-        ],
+        messages: [Promt.testSystem, { role: 'user', content: text }],
       });
       return response.choices[0].message?.content || 'No response generated';
     } catch (error) {
---
+++ src/open-ai/prompt.ts
@@ -0,0 +1,5 @@
+export const testSystem = {
+  role: 'system',
+  content:
+    '너는 openai에 대해 설명해주는 비서야. 주어진 text를 보고 할 수 있는 일에 대해 말해줘.',
+} as const;
