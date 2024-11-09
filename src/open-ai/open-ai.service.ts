57f87392fccbf2570ec312f5d8d6cecb0c16b68d
pig19980 <pig19980@kaist.ac.kr>
2024-11-09 21:04:19 +0900

    feat: OpenAiService에 STT가 부정확한 것을 고쳐주는 함수 추가

--- src/open-ai/open-ai.service.ts
+++ src/open-ai/open-ai.service.ts
@@ -2,7 +2,10 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { OpenAI } from 'openai';
 import * as Promt from './prompt';
-import { VoiceChatting } from 'src/conversation-datas/data/voice-chatting';
+import {
+  VoiceChat,
+  VoiceChatting,
+} from 'src/conversation-datas/data/voice-chatting';
 
 @Injectable()
 export class OpenAiService {
@@ -28,9 +31,71 @@ export class OpenAiService {
     }
   }
 
+  private async beautifyVoiceChatChunk(
+    voiceChatChunk: VoiceChat[],
+  ): Promise<VoiceChat[]> {
+    try {
+      const response = await this.openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        messages: [
+          Promt.beutifySystem,
+          { role: 'user', content: JSON.stringify(voiceChatChunk) },
+        ],
+      });
+      if (!response.choices[0].message) {
+        throw new Error('OpenAi로부터 받은 response가 없음.');
+      }
+      console.log('response', response.choices[0].message.content);
+      return JSON.parse(response.choices[0].message.content);
+    } catch (error) {
+      this.logger.error(error.stack);
+      return voiceChatChunk;
+    }
+  }
+
+  private async beautifyVoiceChats(voiceChats: VoiceChat[]) {
+    let currentChunk: VoiceChat[] = [];
+    let convertedVoiceChats: VoiceChat[] = [];
+    let currentChunkLength = 0;
+    const CHUNK_SIZE = 2000;
+
+    for (const chat of voiceChats) {
+      const messageLength = JSON.stringify(chat).length;
+
+      if (currentChunkLength + messageLength <= CHUNK_SIZE) {
+        currentChunk.push(chat);
+        currentChunkLength += messageLength;
+      } else {
+        convertedVoiceChats = convertedVoiceChats.concat(
+          await this.beautifyVoiceChatChunk(currentChunk),
+        );
+        console.log('converted chats', convertedVoiceChats);
+        currentChunk = [chat];
+        currentChunkLength = messageLength;
+      }
+    }
+
+    if (currentChunk.length > 0) {
+      convertedVoiceChats = convertedVoiceChats.concat(
+        await this.beautifyVoiceChatChunk(currentChunk),
+      );
+    }
+    console.log('final output', convertedVoiceChats);
+    return convertedVoiceChats;
+  }
+
+  private async summaryVoiceChatting(voiceChats: VoiceChat[]) {
+    return 'temp summary';
+  }
+
   async setVoiceChatting(voieChatting: VoiceChatting) {
     // TODO: beautify voice chatting
+    const originalChats = voieChatting.voiceChats;
+    voieChatting.voiceChats = await this.beautifyVoiceChats(originalChats);
     // TODO: summary voice chatting
+    voieChatting.voiceSummary = await this.summaryVoiceChatting(
+      voieChatting.voiceChats,
+    );
     return;
   }
 }
--- src/open-ai/prompt.ts
+++ src/open-ai/prompt.ts
@@ -3,3 +3,15 @@ export const testSystem = {
   content:
     '너는 openai에 대해 설명해주는 비서야. 주어진 text를 보고 할 수 있는 일에 대해 말해줘.',
 } as const;
+
+export const beutifySystem = {
+  role: 'system',
+  content: `아래는 음성에서 텍스트로 변환된 내용입니다. 'message' 항목에 해당되는 부분만 수정해 주세요. 프로그래밍 관련 외래어와 기술 용어가 부정확하게 변환된 부분을 찾아서 올바르게 수정해 주세요.
+변환된 텍스트에서 기술적인 용어가 문맥에 맞게 정확하게 표기되도록 주의해 주세요. 또한, 응답은 input과 같은 형식으로 보내주세요.
+
+
+아래는 input의 예시야.
+[{"date":"2024-11-08T05:34:22.863Z","message":"셰어드 유 아이디 좀 봐줄래?","name":"user5","email":"user5@github.com"}]
+아래는 output의 예시야.
+[{"date":"2024-11-08T05:34:22.863Z","message":"shared uuid 좀 봐줄래?","name":"user5","email":"user5@github.com"}]`,
+} as const;
