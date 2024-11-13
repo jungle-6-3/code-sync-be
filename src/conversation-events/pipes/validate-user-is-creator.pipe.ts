2ea4f83f5e174784f0efa910561a858f429cb1c0
pig19980 <pig19980@kaist.ac.kr>
2024-11-13 11:10:50 +0900

    refactor: creator가 아닐 때 ValidateUserIsCreatorPipe를 지나면, ConversationException을 던지도록 수정

--- src/conversation-events/pipes/validate-user-is-creator.pipe.ts
+++ src/conversation-events/pipes/validate-user-is-creator.pipe.ts
@@ -1,13 +1,17 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { RoomSocket, SocketStatus } from '../interfaces/room-socket.interface';
-import { WsException } from '@nestjs/websockets';
+import { ConversationException } from '../conversation-events.filter';
 
 @Injectable()
 export class ValidateUserIsCreatorPipe implements PipeTransform {
   transform(value: RoomSocket, metadata: ArgumentMetadata) {
     if (value.constructor && value.constructor.name == 'Socket') {
       if (value.status != SocketStatus.CREATOR) {
-        throw new WsException(`방장이 아닙니다.: ${value.status}`);
+        throw new ConversationException(
+          'PERMISSION_1',
+          '권한이 없습니다.',
+          true,
+        );
       }
     }
     return value;
